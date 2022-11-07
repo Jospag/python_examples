65abac3b7809792982c6f634b758b1f2c6c89e68
Emmanuel <jospag4christ@yahoo.com>
2022-11-07 15:09:21 +0100

    Updates

--- nesting_list_and_tuple.py
+++ nesting_list_and_tuple.py
@@ -10,3 +10,19 @@ large_cat = animals[:]
 large_cat.append("Monkey")
 print(large_cat)
 print(animals)
+
+# Sorting list
+
+colours = ['red', 'yellow', 'green', 'blue']
+colours.sort()
+print(colours)
+
+numbers = [1, 10, 5, 8, 15, 20, 11]
+numbers.sort()
+print(numbers)
+
+
+# Sorting string by string length
+colours = ['red', 'yellow', 'green', 'blue']
+colours.sort(key=len)
+print(colours)
