4663e943c1c016ed43b086eefcf150f804f3007f
Emmanuel <jospag4christ@yahoo.com>
2022-11-07 14:47:44 +0100

    Updates

--- nesting_list_and_tuple.py
+++ nesting_list_and_tuple.py
@@ -2,3 +2,11 @@ animals = ["lion", "tiger", "frumious Bandersnatch"]
 large_cat = animals
 large_cat.append("Tiger")
 print(animals)
+
+# To return different object after copying in python
+
+animals = ['lion', 'tiger', 'frumious Bardersnatcch']
+large_cat = animals[:]
+large_cat.append("Monkey")
+print(large_cat)
+print(animals)
