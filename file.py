8357d41cf11a6a96c4d63d0036777daeff3b93f4
Emmanuel <jospag4christ@yahoo.com>
2022-11-09 12:39:43 +0100

    modified file python

---
+++ file.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+path = Path("/home/delight/Documents/wedding program.docx")
+
+pathes = Path.cwd()
+print(pathes.is_absolute())
+
+new_dir = Path.home() / "new_directory"
+# new_dir.mkdir()
+
+file_path = new_dir / "file1.txt"
+# file_path.touch()
+
+for path in new_dir.iterdir():
+    print(path)
