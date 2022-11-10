43c80df0d72d3c6b5c90d0e58b6f6afc45c8e085
Emmanuel <jospag4christ@yahoo.com>
2022-11-10 14:31:30 +0100

    update

--- file.py
+++ file.py
@@ -6,12 +6,12 @@ pathes = Path.cwd()
 print(pathes.is_absolute())
 
 new_dir = Path.home() / "new_directory"
- new_dir.mkdir()
+new_dir.mkdir()
 
 file_path = new_dir / "file1.txt"
- file_path.touch()
+file_path.touch()
 
- for path in new_dir.iterdir():
+for path in new_dir.iterdir():
     print(path)
 
 for path in list(new_dir.iterdir()):
@@ -19,3 +19,12 @@ for path in list(new_dir.iterdir()):
 
 for path in new_dir.glob("*.txt"):
     print(path)
+
+#   writing and reading csv file
+temperature_readings = [68, 65, 68, 70, 74, 72]
+
+file_path = Path.home() / "temperatures.txt"
+with file_path.open(mode="a", encoding="utf-8") as file:
+    file.write(str(temperature_readings[0]))
+    for temp in temperature_readings[1:]:
+        file.write(f",{temp}")
