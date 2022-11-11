532938d60c56599e40f040de461ec65a54e33373
Emmanuel <jospag4christ@yahoo.com>
2022-11-11 13:47:20 +0100

    DB

--- db_connect.py
+++ db_connect.py
@@ -1,12 +1,27 @@
 import sqlite3
 
-connection = sqlite3.connect(":memory:")
-cursor = connection.cursor()
-query = "SELECT datetime('now', 'localtime');"
-time = cursor.execute(query).fetchone()[0]
-print(time)
-
 # with sqlite3.connect("test_database.db") as connection:
-#     ... cursor = connection.cursor()
-# ... query = "SELECT datetime('now', 'localtime');"
-# ... time = cursor.execute(query).fetchone()[0]
+#     cursor = connection.cursor()
+#     query = "SELECT datetime('now', 'localtime');"
+#     time = cursor.execute(query).fetchone()[0]
+#     print(time)
+
+# Actual connection to the db
+connection = sqlite3.connect("test_database.db")
+cursor = connection.cursor()
+cursor.execute(
+    """CREATE TABLE People(
+    FirstName TEXT,
+    LastName TEXT,
+    Age INT
+    );"""
+)
+cursor.execute(
+    """INSERT INTO People VALUES(
+    'Ron',
+    'Obvious',
+    42
+    );"""
+)
+connection.commit()
+connection.close()
